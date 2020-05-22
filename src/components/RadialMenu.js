5149641d5613106a97bf84fb916aeda1f174a5a9
engerb <engerbewza@gmail.com>
2020-05-21 18:56:20 -0700

    Update RadialMenu.js

--- src/components/RadialMenu.js
+++ src/components/RadialMenu.js
@@ -3,11 +3,30 @@ import React from "react";
 class RadialMenu extends React.Component {
     constructor(props) {
         super(props);
-        
+
+        this.state = { 
+            gamePadConnected: false
+        };
+
+        this.gamePad = null;
     }
 
     componentDidMount() {
-        // controller listeners
+        window.addEventListener('gamepadconnected', event => {
+            this.setState({ gamePadConnected: true });
+            this.gamePad = navigator.getGamepads()[event.gamepad.index];
+
+            console.log('Gamepad connected at index %d: %s. %d buttons, %d axes.',
+                this.gamePad.index, this.gamePad.id,
+                this.gamePad.buttons.length, this.gamePad.axes.length);
+        });
+
+        window.addEventListener('gamepaddisconnected', () => {
+            this.setState({ gamePadConnected: false });
+            this.gamePad = null
+
+            console.log('Gamepad disconnected.');
+        });
     }
 
     render() {
