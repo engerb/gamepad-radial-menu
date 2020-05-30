27027fda7d62b23d1160b02cedfee628106921cf
engerb <engerbewza@gmail.com>
2020-05-29 18:52:40 -0700

    Trying react-dat-gui
    need styles

--- src/components/TestSettings.js
+++ src/components/TestSettings.js
@@ -1,16 +1,35 @@
 import React from "react";
+import DatGui, { DatBoolean, DatColor, DatNumber, DatString } from 'react-dat-gui';
 
 class TestSettings extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            data: {
+                package: 'react-dat-gui',
+                power: 9000,
+                isAwesome: true,
+                feelsLike: '#2FA1D6',
+            }
+        }
+
+        this.handleUpdate = newData =>
+            this.setState(prevState => ({
+            data: { ...prevState.data, ...newData }
+        }));
     }
 
     render() {
+        const { data } = this.state;
+
         return (
-            <div className = ''>
-                
-            </div>
+            <DatGui data={data} onUpdate={this.handleUpdate}>
+                <DatString path='package' label='Package' />
+                <DatNumber path='power' label='Power' min={9000} max={9999} step={1} />
+                <DatBoolean path='isAwesome' label='Awesome?' />
+                <DatColor path='feelsLike' label='Feels Like' />
+            </DatGui>
         );
     }
 }
